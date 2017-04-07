cd9b01ff7a68ae98c46f1bb77a5c68fd85cbca24
Enes Çakır <enes@cakir.web.tr>
2017-04-07 16:36:11 +0300

    Before front-end integration

--- invitation/views.py
+++ invitation/views.py
@@ -4,17 +4,15 @@ from invitation.models import Student
 
 # Create your views here.
 
-
 def index(request):
     return HttpResponse("Vue.js Index")
 
 def request(request):
-    first_name=request.POST['first_name']
-    last_name=request.POST['last_name']
+    first_name=request.POST['firstName']
+    last_name=request.POST['lastName']
     email=request.POST['email']
     entrance=request.POST['entrance']
     department="CMPE"
-
     student = Student(name= first_name + " " + last_name, email=email, entrance=entrance, deparment=deparment)
     student.save()
     return HttpResponse(student)
