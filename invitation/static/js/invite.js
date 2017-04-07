a9d23dd5976f26961b184ac611e14f9d7188494a
Enes Çakır <enes@cakir.web.tr>
2017-04-07 23:27:17 +0300

    Invite ajax added

--- invitation/static/js/invite.js
+++ invitation/static/js/invite.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
   $(".invite").click(function(event){
     event.preventDefault()
-    alert("The paragraph was clicked.");
+    var button = $(this)
+    var studentID = button.attr("student-id")
+    $.ajax({
+        url : "/request/" + studentID + "/invite",
+        type: "POST",
+        success: function(data, textStatus, jqXHR)
+        {
+          alert("The paragraph was clicked.");
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        {
+     
+        }
+    });
   });
 });
