f90d4f83051864cf5545438e0e100ec1b5be8090
Enes Çakır <enes@cakir.web.tr>
2017-04-07 21:23:59 +0300

    manage.py collectstatic runned

--- cmpeslack/settings.py
+++ cmpeslack/settings.py
@@ -122,4 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
