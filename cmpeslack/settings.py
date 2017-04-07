bf43dff4224d2dbe7552ffa79a4d678cded33f1c
Enes Çakır <enes@cakir.web.tr>
2017-04-07 21:58:36 +0300

    Hardcoded env removed

--- cmpeslack/settings.py
+++ cmpeslack/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'cmpeslack.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cmpeslack',
-        'USER': 'postgres',
-        'PASSWORD': 'akcakavak',
-        'HOST': '127.0.0.1',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USERNAME'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
         'PORT': '5432',
     }
 }
